626eb2198fbab4ba72d758e3cea44331b2adecba
John Bokma <contact@johnbokma.com>
2019-04-04 18:58:37 +0200

    Use JSON::XS class, not JSON

--- tumblelog.pl
+++ tumblelog.pl
@@ -307,7 +307,7 @@ sub create_json_feed {
         items => \@items,
     };
     my $path = $options->{ 'feed-path' };
-    my $json = JSON->new->utf8->pretty->canonical->encode( $feed );
+    my $json = JSON::XS->new->utf8->pretty->canonical->encode( $feed );
     path( "$options->{ 'output-dir' }/$path" )->spew_raw( $json );
     $options->{ quiet } or print "Created '$path'\n";
 
