66fe546dd521369ebb748b50e2ebe91dc77aa27f
John Bokma <contact@johnbokma.com>
2021-06-19 17:41:13 +0200

    Bump version to 5.1.2

--- tumblelog.py
+++ tumblelog.py
@@ -22,7 +22,7 @@ except ImportError:
 import commonmark
 import commonmark.node
 
-VERSION = '5.1.1'
+VERSION = '5.1.2'
 
 RE_DATE_TITLE = re.compile(r'(\d{4}-\d{2}-\d{2})(.*?)\n(.*)', flags=re.DOTALL)
 RE_AT_PAGE_TITLE = re.compile(
